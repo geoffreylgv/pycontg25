d4e030ce680d05e9421461f2710638d63e1e16ce
Geoffrey Logovi <52314615+geoffreylgv@users.noreply.github.com>
2025-05-19 13:43:27 +0000

    ui: update contact page for sponsor (the phone number should be with country code) ; edit the mastodon to have rel me inside

--- static/js/sponsor.js
+++ static/js/sponsor.js
@@ -8,3 +8,29 @@
 //     }
 // });
 
+document.addEventListener("DOMContentLoaded", function () {
+    const input = document.querySelector("#contact-phone");
+
+    const iti = window.intlTelInput(input, {
+        initialCountry: "auto",
+        utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@17/build/js/utils.js",
+        geoIpLookup: function (callback) {
+            fetch('https://ipapi.co/json')
+                .then(res => res.json())
+                .then(data => callback(data.country_code))
+                .catch(() => callback('TG'));
+        },
+        nationalMode: false,
+        preferredCountries: ['tg', 'bj', 'gh', 'fr', 'us']
+    });
+
+    const form = document.querySelector("#sponsor-form");
+    form.addEventListener("submit", function (e) {
+        if (iti.isValidNumber()) {
+            input.value = iti.getNumber();
+        } else {
+            alert("Please enter a valid phone number.");
+            e.preventDefault();
+        }
+    });
+});
